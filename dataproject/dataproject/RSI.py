a8f0e6771ccd749d9a9cd44d6c5681016ff673ca
Fandango88 <Johansen88@gmail.com>
2019-03-28 21:23:52 +0100

    Upload RSI

--- dataproject/dataproject/RSI.py
+++ dataproject/dataproject/RSI.py
@@ -1,30 +1,83 @@
 ## pip install plotly  <-- install inorder to show the plot type
+## pip install talib
+
 #%%
-import plotly
+import pandas_datareader
 import plotly.plotly as py
 import plotly.graph_objs as go
-from datetime import datetime
+import talib 
+##import cufflinks as cf
 import pandas as pd
-import pandas_datareader.data as web
-
+from datetime import datetime
 #%%
-Stock = pd.read_csv('TSLA', parse_dates=True, index_col=0)
 
 
-Stock.reset_index(inplace=True)
-Stock.set_index("date", inplace=True)
-#%%½
-Stock.columns = [col.replace('TSLA', '') for col in Stock.columns]
 #%%
-trace = go.Scatter(x=list(Stock.date),
-                   y=list(Stock.high))
-#%%
-Stock.head(n=20)
+start = datetime(2018,1,1)
+end = datetime(2018,12,31)
+ticker = 'TSLA'
 
 #%%
-data = [trace]
+def prices(name, start=start, end=end):
+    '''returns a dataframe with stock-information for a given company'''
+    return pandas_datareader.iex.daily.IEXDailyReader(name, start, end).read()
+
+
+tesla = prices(ticker, start,end)
+tesla.reset_index(inplace=True)
+
+
+#%%
+close = tesla['close'].values
+#%%
+rsi = talib.RSI(close, timeperiod = 14)
+
+
+tesla["RSI"]= rsi
+tesla["Overbought"] = 70
+tesla["Oversold"] = 30
+#%%
+tesla.head(n=20)
+#%%
+help(go.Scatter)
+#%%
+trace1 = go.Scatter(
+    y=tesla['RSI'],
+    x=tesla['date'],    
+    name = "RSI",
+    line = dict(color = '#7F7F7F'),
+    )
+
+#%%
+trace2 = go.Scatter(
+    y=tesla['Overbought'],
+    x=tesla['date'],   
+    name = "Overbought",
+    line = dict(color = '#63c442'),
+    hoverinfo='none'
+    )
+#%%
+trace3 = go.Scatter(
+    y=tesla['Oversold'],
+    x=tesla['date'],   
+    name = "Oversold",
+    line = dict(color = '#705d65'),
+    hoverinfo='none'
+    )
+#%%
+trace4 = go.Scatter(
+    y=tesla['close'],
+    x=tesla['date'],   
+    name = "Price",
+    line = dict(color = '#2ed362'),
+    )
+
+
+#%%
+data = [trace1,trace2,trace3,trace4]
+#%%
 layout = dict(
-    title='Time series with range slider and selectors',
+    title='Nvidia with RSI',
     xaxis=dict(
         rangeselector=dict(
             buttons=list([
@@ -36,15 +89,11 @@ layout = dict(
                      label='6m',
                      step='month',
                      stepmode='backward'),
-                dict(count=1,
-                    label='YTD',
-                    step='year',
-                    stepmode='todate'),
-                dict(count=1,
-                    label='1y',
-                    step='year',
-                    stepmode='backward'),
+                dict(count=12,
+                     label = '1 year',
+                     stepmode='backward'),
                 dict(step='all')
+
             ])
         ),
         rangeslider=dict(
@@ -55,4 +104,4 @@ layout = dict(
 )
 #%%
 fig = dict(data=data, layout=layout)
-py.iplot(fig)
+py.iplot(fig, filename = "Nvidia1")
