8c7b25b70948a38ffcdf5d08e9603a4bf1539be5
Fandango88 <Johansen88@gmail.com>
2019-04-01 10:19:21 +0200

    New RSI code

--- dataproject/dataproject/functions.py
+++ dataproject/dataproject/functions.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 start = datetime(2018,1,2)
 end = datetime(2018,3,31)
-
+pd.__version__
 #%%
 def prices(name, start=start, end=end):
     '''    
@@ -80,5 +80,30 @@ def RSI(series, period):
 
 
 
+def rsiFunc(prices, n=14):
+    deltas = np.diff(prices)
+    seed = deltas[:n+1]
+    up = seed[seed>=0].sum()/n
+    down = -seed[seed<0].sum()/n
+    rs = up/down
+    rsi = np.zeros_like(prices)
+    rsi[:n] = 100. - 100./(1.+rs)
 
+    for i in range(n, len(prices)):
+        delta = deltas[i-1] # cause the diff is 1 shorter
+
+        if delta>0:
+            upval = delta
+            downval = 0.
+        else:
+            upval = 0.
+            downval = -delta
+
+        up = (up*(n-1) + upval)/n
+        down = (down*(n-1) + downval)/n
+
+        rs = up/down
+        rsi[i] = 100. - 100./(1.+rs)
+
+    return rsi
     
